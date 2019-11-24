99db77927b3e6c897278d2f6224160299386b20d
Vess <vesselofgod@naver.com>
2019-11-24 15:18:52 +0900

    Add files via upload

---
+++ Chapter6/example code.py
@@ -0,0 +1,19 @@
+import sqlite3
+conn = sqlite3.connect("test.db")#여기 안에 DB이름을 쓰면 됨.
+
+#declare cursor
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
